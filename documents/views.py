c4b94f62928b5304300d8afad715444ebe337514
Stalin <stalin_dany98@hotmail.com>
2020-12-01 01:13:12 -0500

    Imprimir resultados desde la api

--- documents/views.py
+++ documents/views.py
@@ -1,6 +1,31 @@
 # Django
 from django.shortcuts import render, redirect
 
+from serpwow.google_search_results import GoogleSearchResults
+import json
+
+
+def getBooks():
+    serpwow = GoogleSearchResults("BB5814F4D0CA4B85A0E59711D4B0F1FB")
+
+    params = {
+        "q": "machine learning",
+        "search_type": "scholar",
+        "hl": "en"
+    }
+
+    result = serpwow.get_json(params)
+    return result['scholar_results']
+
 
 def home(request):
     return render(request, 'index.html')
+
+
+def result(request):
+    data = getBooks()
+    for i in data:
+        i['title'] = i['title'].replace('[PDF][PDF]', '')
+        i['title'] = i['title'].replace('[BOOK][B]', '')
+        
+    return render(request, 'result.html', {'data': data})
--- proyect_ml/urls.py
+++ proyect_ml/urls.py
@@ -6,4 +6,5 @@ from documents import views as documents_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', documents_views.home),
+    path('search/', documents_views.result),
 ]

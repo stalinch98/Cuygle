96d7b8b6b84f7e669ba7c34f83a7da8eff02efd4
stalin <stalin_dany98@hotmail.com>
2020-12-07 18:32:56 -0500

    inicio gpt2

--- documents/views.py
+++ documents/views.py
@@ -11,6 +11,9 @@ import re
 import nltk
 import numpy as np
 import math
+import transformers
+import torch
+
 from time import time
 
 nltk.download('stopwords')
@@ -24,6 +27,39 @@ STEMMER = PorterStemmer()
 # Constantes con la ponderacion para los datos
 VAL_TITLES = 0.1
 VAL_ABSTRACTS = 0.9
+
+# Inicio Gpt2
+gpt_tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2-large')
+gpt_model = transformers.GPT2LMHeadModel.from_pretrained('gpt2-large')
+
+
+def gen_text(prompt_text, tokenizer, model, n_seqs=1, max_length=25):
+    encoded_prompt = tokenizer.encode(
+        prompt_text, add_special_tokens=False, return_tensors="pt")
+    output_sequences = model.generate(
+        input_ids=encoded_prompt,
+        max_length=max_length+len(encoded_prompt),
+        temperature=1.0,
+        top_k=0,
+        top_p=0.9,
+        repetition_penalty=1.2,
+        do_sample=True,
+        num_return_sequences=n_seqs
+    ) 
+    if len(output_sequences.shape) > 2:
+        output_sequences.squeeze_()
+    generated_sequences = []
+    for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
+        generated_sequence = generated_sequence.tolist()
+        text = tokenizer.decode(generated_sequence)
+        total_sequence = (
+            prompt_text +
+            text[len(tokenizer.decode(encoded_prompt[0],
+                                      clean_up_tokenization_spaces=True, )):]
+        )
+        generated_sequences.append(total_sequence)
+    return generated_sequences
+# Fin Gpt2
 
 # Inicio clasificador
 
@@ -219,5 +255,10 @@ def result(request):
     final = matriz_distancias(dis_tit, dis_abst)
 
     similar_top = top(final)
+    val = gen_text("Hi my name is stalin and",
+         gpt_tokenizer,
+         gpt_model,
+         max_length=50)
+    print(val[0])
 
     return render(request, 'result.html', {'data': data, 'similar_top': similar_top})
